23b12b8513c22d3c4fe4fccdb354f82d974aa967
zhangdasen <zhangdasen@126.com>
2017-02-26 22:44:56 +0800

    Add Test

---
+++ Test项目/Classes/Test.h
@@ -0,0 +1,12 @@
+//
+//  Test.h
+//  Test项目
+//
+//  Created by 张大森 on 2017/2/26.
+//  Copyright © 2017年 张大森. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface Test : NSObject
++ (void)flog;
+@end
---
+++ Test项目/Classes/Test.m
@@ -0,0 +1,19 @@
+
+//
+//  Test.m
+//  Test项目
+//
+//  Created by 张大森 on 2017/2/26.
+//  Copyright © 2017年 张大森. All rights reserved.
+//
+
+#import "Test.h"
+
+#import <UIKit/UIKit.h>
+
+@implementation Test
++ (void)flog
+{
+    NSLog(@"logo logo");
+}
+@end
